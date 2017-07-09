abf3c6a0330cc29286a0e5e706722db10199edc7
Gabriel Mičko <micsko@gmail.com>
2017-07-09 21:08:38 +0200

    Update according to 1

---
+++ README1.md
@@ -0,0 +1,33 @@
+## GraphQL Workshop
+
+### Task 1
+
+#### Install dependencies
+```bash
+yarn install
+```
+
+I've created a file in "src/model/file.js". Later on this file will help you to feed the GraphQL resolvers.
+Meaning that it will return some data according to given parameters.
+Your task is to write the function logic according to the documentation.
+
+#### Hints:
+- "src/db" has two JSON files which you need.
+- You might want to use .filter function in some of the functions.
+- All of the function are synchrounous.
+
+
+#### Testing:
+When you are done, please run this command:
+
+```bash
+yarn run test
+```
+
+Or if you want to run the test on every change run this command:
+
+```bash
+yarn run test:watch
+```
+
+##### Good luck!
--- src/model/file.js
+++ src/model/file.js
@@ -1,16 +1,29 @@
 import speakers from '../db/speakers.json';
 import talks from '../db/talks.json';
 
+/**
+ * Returns all the talks
+ * @returns {Array} Array containing the talks talks
+ */
 const getTalks = () => {
   return talks;
 };
 
+/**
+ * Returns all the speakers according to the arguments
+ * If there are no arguments the function returns all
+ * of the speakers. Only one argument is going to be passed
+ * at a time.
+ * @param  {String} args.id id of a speaker OR
+ * @param  {String} args.name name of a speaker
+ * @return {Array} Returns the filtered ot all of the speakers
+ */
 const getSpeakers = args => {
   if (args) {
     return speakers.filter(speaker => {
       if (args.id) {
         return speaker.id === args.id;
-      } else if (speaker.id === args.name) {
+      } else if (speaker.name === args.name) {
         return speaker;
       }
     });
@@ -18,6 +31,12 @@ const getSpeakers = args => {
   return speakers;
 };
 
+/**
+ * Get all of the talks by a speaker id.
+ * @param  {String} speakerId [description]
+ * @return {Array} Returns all of the talks where the
+ * speaker_id's value matches the given argument.
+ */
 const getTalksBySpeakerId = speakerId => {
   return getTalks().filter(talk => {
     return talk.speaker_id === speakerId;
