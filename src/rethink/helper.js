f8c67ad7b6328626c9b17c82cecc62ac0f5d1d15
Gabriel Mičko <micsko@gmail.com>
2017-07-11 22:02:55 +0200

    Rethink helper solution

--- src/rethink/helper.js
+++ src/rethink/helper.js
@@ -5,7 +5,13 @@ import r from 'rethinkdb';
  * The connection details are defined in this function.
  * @return {Promise<Object: conn>} Returns a Promise with a connnection
  */
-const connectToDB = () => {};
+const connectToDB = () => {
+  return r.connect({
+    host: 'localhost',
+    port: 28015,
+    user: 'admin'
+  });
+};
 
 /**
  * Creates a database according to the given argument.
@@ -13,7 +19,13 @@ const connectToDB = () => {};
  * @param  {String} dbName Database name
  * @return {Promise} A promise which resolves when database created, rejects on any error
  */
-const createDatabase = (conn, dbName) => {};
+const createDatabase = (conn, dbName) => {
+  return new Promise(resolve => {
+    r.dbCreate(dbName).run(conn, err => {
+      err ? resolve(err) : resolve();
+    });
+  });
+};
 
 /**
  * Creates a table in the databse.
@@ -22,7 +34,13 @@ const createDatabase = (conn, dbName) => {};
  * @param  {String} tableName Name of the new table
  * @return {Promise} A promise which resolves when the table is created, rejects on any error
  */
-const createTable = (conn, dbName, tableName) => {};
+const createTable = (conn, dbName, tableName) => {
+  return new Promise(resolve => {
+    r.db(dbName).tableCreate(tableName).run(conn, err => {
+      err ? resolve(err) : resolve();
+    });
+  });
+};
 
 /**
  * Inserts data to the table. Checks if the dataArray argument is a valid array and its content is not empty.
@@ -34,7 +52,24 @@ const createTable = (conn, dbName, tableName) => {};
  * @param  {Array} dataArray Array with Objects containing data to be inserted
  * @return {Promise} Promise which resolves when inserts are done.
  */
-const insertData = (conn, dbName, tableName, dataArray) => {};
+const insertData = (conn, dbName, tableName, dataArray) => {
+  let insertPromisesArray = [];
+  if (dataArray && Array.isArray(dataArray)) {
+    dataArray.forEach(data => {
+      let insertPromise = new Promise((resolve, reject) => {
+        r.db(dbName).table(tableName).insert(data).run(conn, (err, result) => {
+          if (!err && result.inserted === 1) {
+            resolve(result);
+          } else {
+            reject(err);
+          }
+        });
+      });
+      insertPromisesArray.push(insertPromise);
+    });
+  }
+  return Promise.all(insertPromisesArray);
+};
 
 /**
  * Get all data from a table according to a filter object.
@@ -44,7 +79,13 @@ const insertData = (conn, dbName, tableName, dataArray) => {};
  * @param  {Object} filters Object of containing the filter (ie. {speaker_id: 10}, check: toArray())
  * @return {Promise} Promise which resolves with the filtered data.
  */
-const getByFilters = (conn, dbName, tableName, filters) => {};
+const getByFilters = (conn, dbName, tableName, filters) => {
+  return new Promise((resolve, reject) => {
+    r.db(dbName).table(tableName).filter(filters).run(conn, (err, result) => {
+      !err ? resolve(result.toArray()) : reject(err);
+    });
+  });
+};
 
 /**
  * Get all the data from a table.
@@ -53,7 +94,13 @@ const getByFilters = (conn, dbName, tableName, filters) => {};
  * @param  {String} tableName Table name
  * @return {Promise} Promise which resolves with the data.
  */
-const getAll = (conn, dbName, tableName) => {};
+const getAll = (conn, dbName, tableName) => {
+  return new Promise((resolve, reject) => {
+    r.db(dbName).table(tableName).run(conn, (err, result) => {
+      !err ? resolve(result.toArray()) : reject(err);
+    });
+  });
+};
 
 /**
  * Deletes all the content from tables.
@@ -64,7 +111,19 @@ const getAll = (conn, dbName, tableName) => {};
  * @param  {Array} tables Array containing tables (strings), which you want to wipe (ie: ["speakers", "talks"])
  * @return {Promise} Promise which resolves when deletes are done.
  */
-const wipeTables = (conn, dbName, tables) => {};
+const wipeTables = (conn, dbName, tables) => {
+  let deletePromises = [];
+  tables.forEach(table => {
+    let delPromise = new Promise(resolve => {
+      r.db(dbName).table(table).delete().run(conn, (conn, err) => {
+        err ? resolve(err) : resolve();
+      });
+    });
+
+    deletePromises.push(delPromise);
+  });
+  return Promise.all(deletePromises);
+};
 
 export {
   connectToDB,
